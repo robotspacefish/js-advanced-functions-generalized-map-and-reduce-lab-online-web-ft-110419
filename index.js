aa528c4feefe3c7984ff10a36101547b9de05790
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-04 09:47:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(sourceArray, callback) {
+  const newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(callback(sourceArray[i]));
+  }
+  return newArray;
+}
+
+function reduce(sourceArray, callback, startingPoint) {
+  let total;
+  let i;
+  if (startingPoint) {
+    total = startingPoint;
+    i = 0;
+  } else {
+    total = sourceArray[0];
+    i = 1;
+  }
+
+  for (i; i < sourceArray.length; i++) {
+    total = callback(sourceArray[i], total);
+  }
+  return total;
+}
